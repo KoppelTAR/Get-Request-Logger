95ce6123ae796e812c66fc26dce86c5692bda7ae
Opilane TTHK <opilane@tthk.ee>
2022-01-05 11:53:57 +0200

    Finale

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,32 +1,22 @@
-const { json } = require('express');
 const fs = require('fs');
+const date = require('../model/getdate.js');
 const path = require('path');
 const filePath = path.join(path.dirname(require.main.filename), 'data', 'log.json');
 
 
 exports.getMainPage = (request, response)=>{
-
-    
     let data = fs.readFileSync(filePath);
     let obj = JSON.parse(data);
-    var currentdate = new Date(); 
-    var today = + currentdate.getHours() + ":"  
-    + currentdate.getMinutes() + ":" 
-    + currentdate.getSeconds() + " "+
-    currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear();
-                
+    let time =  date.getDate();
     let newVisit = {
-        visitTime: today,
+        visitTime: time,
     };
     obj.push(newVisit);
     JSON.stringify(obj);
-        fs.writeFile(filePath, JSON.stringify(obj), (error) =>{
-            log = fs.readFileSync(filePath);
-            log = JSON.parse(log);
-            console.log(log);
-            response.render('index', {currentVisits: log});
-        });
+    fs.writeFile(filePath, JSON.stringify(obj), (error) =>{
+        log = fs.readFileSync(filePath);
+        log = JSON.parse(log);
+        response.render('index', {currentVisits: log});
+    });
         
     }
